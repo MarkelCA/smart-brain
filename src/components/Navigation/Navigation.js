d84d5cec70833caefea3307e747269205b97ffbd
MarkelCA <cuestaarribas.markel@gmail.com>
2021-06-17 17:22:45 +0200

    first modifications and created Navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+const Navigation = () => {
+    return (
+        <nav style={{display: 'flex', justifyContent: "flex-end", padding: '.5em' }}>
+            <p>Sign out</p>
+
+        </nav>
+
+    )
+}
+
+export default Navigation
