5e5ff816af0db4f5c9fcb0b37beb66d1d967f688
Markel Cuesta <cuestaarribas.markel@gmail.com>
2022-02-17 09:27:04 +0100

    replaced SignIn class with hooks

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,83 +1,70 @@
 import './SignIn.css'
 import Utils from '../../utils/Utils'
-import React from 'react'
-class SignIn extends React.Component {
-    constructor(props) {
-        super()
-        this.state = {
-            signInEmail : "",
-            signInPassword : "",
-            validLogin : true
-        }
-    }
+import React, {useState} from 'react'
 
-    onEmailChange = (event) => {
-        this.setState({ signInEmail : event.target.value })
-    }
+const SignIn = ({onSubmit, onRegister, loadUser, onRouteChange}) =>  {
 
-    onPasswordChange = (event) => {
-        this.setState({signInPassword : event.target.value})
-    }
+    const [ signInEmail, changeEmail ] = useState('')
+    const [ signInPassword, changePassword ] = useState('')
 
-    onSubmitSignIn = async (e) => {
+    const [ validLogin, submit ] = useState(true)
+
+
+    const onSubmitSignIn = async (e) => {
         e.preventDefault()
 
         const user = await Utils.post('http://localhost:3000/signin', {'Content-Type' : 'application/json'}, {
-            email   : this.state.signInEmail,
-            password: this.state.signInPassword
+            email   : signInEmail,
+            password: signInPassword
         })
 
-        this.setState({validLogin : user == null})
+        submit(user == null)
 
         if(user) {
              delete user.password
-             this.props.loadUser(user)
-             this.props.onRouteChange('home')
+             loadUser(user)
+             onRouteChange('home')
          } 
 
     }
 
-    render() {
-        const { onSubmit, onRegister } = this.props
-        const { validLogin } = this.state
-        return (
-            <form id='signin-form' className="bg-grey-lighter flex flex-col sm:w-full h-auto m-auto" onSubmit={this.onSubmitSignIn}>
-                        <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                            <div className="px-6 py-8 rounded shadow-xl text-black w-full">
-                                <h1 className="text-white mb-8 text-3xl text-center">Sign in</h1>
-                                <input 
-                                    type="text"
-                                    className="block border border-grey-light w-full p-3 rounded mb-4"
-                                    name="email"
-                                    placeholder="Email"
-                                    onChange={this.onEmailChange} />
-                                    
+    return (
+        <form id='signin-form' className="bg-grey-lighter flex flex-col sm:w-full h-auto m-auto" onSubmit={onSubmitSignIn}>
+                    <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                        <div className="px-6 py-8 rounded shadow-xl text-black w-full">
+                            <h1 className="text-white mb-8 text-3xl text-center">Sign in</h1>
+                            <input 
+                                type="text"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="email"
+                                placeholder="Email"
+                                onChange={(e) => changeEmail(e.target.value) }/>
+                                
 
-                                <input 
-                                    type="password"
-                                    className="block border border-grey-light w-full p-3 rounded mb-4"
-                                    name="password"
-                                    placeholder="Password" 
-                                    onChange={this.onPasswordChange} />
+                            <input 
+                                type="password"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="password"
+                                placeholder="Password" 
+                                onChange={ (e) => changePassword(e.target.value) } />
 
-                                <button
-                                    id='submit-form'
-                                    type="submit"
-                                    className="w-full text-center py-3 rounded bg-blue-600 text-gray-800 hover:text-white focus:text-white hover:bg-green-dark focus:outline-none my-1"
-                                    >Log in</button>
-                                { !validLogin ? (<p className='passwordsDontMatch'>Wrong email or password ❌</p>) : ''}
-                            </div>
-
-                            <div className="text-grey-dark mt-6 bg-white px-2 py-5 rounded-lg shadow-md text-black w-full">
-                                You don't have an account? 
-                                <a className="text-lg no-underline border-b text-blue-700 transition duration-200 hover:border-blue-700 text-blue ml-2" href="../login/" onClick={onRegister}>
-                                    Register
-                                </a>.
-                            </div>
+                            <button
+                                id='submit-form'
+                                type="submit"
+                                className="w-full text-center py-3 rounded bg-blue-600 text-gray-800 hover:text-white focus:text-white hover:bg-green-dark focus:outline-none my-1"
+                                >Log in</button>
+                            { !validLogin ? (<p className='passwordsDontMatch'>Wrong email or password ❌</p>) : ''}
                         </div>
-                    </form>
-        )
-    }
+
+                        <div className="text-grey-dark mt-6 bg-white px-2 py-5 rounded-lg shadow-md text-black w-full">
+                            You don't have an account? 
+                            <a className="text-lg no-underline border-b text-blue-700 transition duration-200 hover:border-blue-700 text-blue ml-2" href="../login/" onClick={onRegister}>
+                                Register
+                            </a>.
+                        </div>
+                    </div>
+                </form>
+    )
 
 }
 
