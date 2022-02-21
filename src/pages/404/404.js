4f98c64b00a43979b5d146f87a597bbad419f129
MarkelCA <cuestaarribas.markel@gmail.com>
2022-02-21 20:34:17 +0100

    added button to go back on 404

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,8 +1,26 @@
 import './404.css'
+import { Link } from "react-router-dom";
 const PageNotFound = () => {
     return (
-        <div className='flex justify-center items-center h-screen'>
-            <h1 className='not-found-title text-3xl bg-white p-4 py-8 rounded-md text-blue-600'>404 - Page not found</h1>
+        <div className='flex flex-col justify-center items-center h-screen'>
+        <div className="container-404 rounded-md">
+            <h1 className='not-found-title text-3xl border-b-2 border-pink-400 p-4 py-8  text-blue-600'>404 - Page not found</h1>
+            <button className=" 
+            m-4 text-xl 
+            border-b-2 
+            border-blue-600
+            p-2
+            transform
+            text-blue-700
+            hover:text-pink-700
+            hover:scale-110
+            hover:border-pink-700
+            active:scale-100
+            transition
+            duration-10
+                "><Link to="/">⬅️ Go back to main page</Link></button>
+                        
+        </div>
         </div>
     )
 }
